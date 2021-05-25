97ff1b76fb9bc3a7be56ead524977ac529a45de5
安可 <i@h3i.top>
2021-05-25 20:55:03 +0800

    创建 test1.py

---
+++ test1.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+#input("\n\n按下 Enter 键退出。")
+
+import sys; x = 'runoob'; sys.stdout.write(x + '\n')
+
+
+x = "a"
+y = "b"
+# 换行输出
+print(x)
+print(y)
+
+print("------")
+
+# 不换行输出
+print(x,end=" ")
+print(y,end=" ")
+print()
